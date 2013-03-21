c07aadcf186eafad97c88d6d7e43feeda4f6f3f4
Andreas Nilsson <anilsson@redhat.com>
2013-03-21 09:54:08 -0300

    custom login image featuring the guadec logo

--- wordcamp-base/functions.php
+++ wordcamp-base/functions.php
@@ -9,4 +9,10 @@ wcb_maybe_define( 'WCB_URL', get_template_directory_uri() );
 
 require_once "lib/class-wcb-manager.php";
 
+
+function login_css() {
+	wp_enqueue_style( 'login_css', get_template_directory_uri() . '/css/login.css' );
+}
+add_action('login_head', 'login_css');
+
 ?>
