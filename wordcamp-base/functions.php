e369cbd2322b35ad3fc486942fbe56a2c005e680
Andreas Nilsson <anilsson@redhat.com>
2013-03-21 14:16:53 -0300

    more debug

--- wordcamp-base/functions.php
+++ wordcamp-base/functions.php
@@ -1,13 +1,9 @@
 <?php
 
-echo "login start";
-
 function login_css() {
 	wp_enqueue_style( 'login_css', get_template_directory_uri() . '/css/login.css' );
 }
 add_action('login_head', 'login_css');
-
-echo "login end";
 
 require_once "custom/custom.php";
 
