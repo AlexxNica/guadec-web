3cd787930c33af887a7216d38087623b16b71f3e
Andreas Nilsson <anilsson@redhat.com>
2013-03-21 16:48:34 -0300

    corrent path for favicon

--- wordcamp-base/header.php
+++ wordcamp-base/header.php
@@ -7,7 +7,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
- <link rel="shortcut icon" href="theme/images/favicon.png">
+ <link rel="shortcut icon" href="wp-content/themes/images/favicon.png">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <?php wcb_title_tag(); ?>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
