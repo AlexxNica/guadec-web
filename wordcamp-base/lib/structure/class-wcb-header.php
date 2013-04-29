0bf0297820655b3a3c01d166732530c9c7db7433
Andreas Nilsson <anilsson@redhat.com>
2013-04-29 10:27:05 -0300

    added navigation menu

--- wordcamp-base/lib/structure/class-wcb-header.php
+++ wordcamp-base/lib/structure/class-wcb-header.php
@@ -7,13 +7,17 @@ class WCB_Header extends WCB_Element {
 
 	function content() { ?>
 		<div id="<?php echo $this->get_id(); ?>">
-			<div class="grid_4 event-location">Brno, Czech Republic</div>
-			<div class="grid_4 event-logo">&nbsp;</div>
-			<div class="grid_4 event-date">August 1 - 8, 2013</div>
-			<div class="clear"></div>
-			<div class="grid_12 branding-buildings">&nbsp;</div>
+            <div class="event-logo">
+            	<a class="event-logo-link" href="<?php echo home_url(); ?>"></a>
+            </div>
+            <?php 
+				wp_nav_menu(array(		
+						'menu'=>'main',
+						'container_class'=>'main_navigation')
+				); 
+			?>
 		</div><!-- #header -->
-	<?php
+    <?php
 	}
 }
 
