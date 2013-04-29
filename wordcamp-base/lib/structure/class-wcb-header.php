092d48885e5781e3d3f74c2ddb20c2e77225f971
Rui Matos <tiagomatos@gmail.com>
2013-04-30 00:46:58 +0200

    Add the buildings banner back to the header

--- wordcamp-base/lib/structure/class-wcb-header.php
+++ wordcamp-base/lib/structure/class-wcb-header.php
@@ -6,17 +6,16 @@ class WCB_Header extends WCB_Element {
 	}
 
 	function content() { ?>
-		<div id="<?php echo $this->get_id(); ?>">
-            <div class="event-logo">
-            	<a class="event-logo-link" href="<?php echo home_url(); ?>"></a>
-            </div>
-            <?php 
-				wp_nav_menu(array(		
-						'menu'=>'main',
-						'container_class'=>'main_navigation')
-				); 
-			?>
-		</div><!-- #header -->
+	<div id="<?php echo $this->get_id(); ?>">
+		<div class="event-logo">
+			<a class="event-logo-link" href="<?php echo home_url(); ?>"></a>
+		</div>
+		<?php wp_nav_menu(array('menu'=>'main',
+					'container_class'=>'main_navigation'));
+		?>
+		<div class="clear"></div>
+		<div class="grid_12 branding-buildings">&nbsp;</div>
+	</div>
     <?php
 	}
 }
