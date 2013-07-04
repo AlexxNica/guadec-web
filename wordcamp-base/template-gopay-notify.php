4f2d76b2a34909644867447c186654b067545e53
Rui Matos <tiagomatos@gmail.com>
2013-07-04 10:00:30 +0200

    Don't redirect on the gopay notifications page

--- wordcamp-base/template-gopay-notify.php
+++ wordcamp-base/template-gopay-notify.php
@@ -6,13 +6,15 @@ Template Name: GoPay Notify
 require_once('registration.php');
 
 if (is_post() || !check_gopay_params()) {
-  error_page('spurious gopay notify page hit', get_site_url());
+  error_log('spurious gopay notify page hit');
+  exit;
 }
 
 $reg = get_registration_data_for_payment_session_id($_GET['paymentSessionId']);
 
 if ($reg->completed) {
-  error_page("gopay notification for already paid registration $reg->ID", get_site_url());
+  error_log("gopay notification for already paid registration $reg->ID");
+  exit;
 }
 
 maybe_finish_payment($reg);
