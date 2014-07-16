fe58dd23a1ce0653e5c39a56013d91fedf856d29
Pascal Terjan <pterjan@gmail.com>
2014-07-16 09:23:29 +0000

    Add list of participants

---
+++ guadec/participants.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: List of participants
+*/
+?>
+<?php
+global $wpdb;
+require_once("header.php");
+echo "<table class='regtable'><tr><th>Name</th><th>Location</th></th></tr>";
+$result = $wpdb->get_results("SELECT name, country FROM wp_guadec2014_registrations WHERE public = 'YES' AND payment = 'Completed' OR payment ='NoPayment'", ARRAY_A);
+foreach($result as $results){
+	echo "<tr>";
+	echo "<td>"; echo $results['name']; echo "</td>";
+	echo "<td>"; echo $results['country']; echo "</td>";
+	echo "</tr>";
+}
+echo "</table>";
+require_once("footer.php");
+?>
