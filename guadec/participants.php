bd441b3244dc9d657d45192fe617284947d742b1
Alexandre Franke <alexandre.franke@gmail.com>
2014-07-21 23:26:26 +0200

    Also display people who pay on site

--- guadec/participants.php
+++ guadec/participants.php
@@ -7,7 +7,7 @@ Template Name: List of participants
 global $wpdb;
 require_once("header.php");
 echo "<table class='regtable'><tr><th>Name</th><th>Location</th></th></tr>";
-$result = $wpdb->get_results("SELECT name, country FROM wp_guadec2014_registrations WHERE ispublic = 'YES' AND (payment = 'Completed' OR payment ='NoPayment')", ARRAY_A);
+$result = $wpdb->get_results("SELECT name, country FROM wp_guadec2014_registrations WHERE ispublic = 'YES' AND (payment = 'Completed' OR payment ='NoPayment' OR payment ='OnSite')", ARRAY_A);
 foreach($result as $results){
 	echo "<tr>";
 	echo "<td>"; echo $results['name']; echo "</td>";
